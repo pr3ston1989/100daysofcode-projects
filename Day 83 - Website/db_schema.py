09448f7887b4207303b2a333d66995bd051e90cd
Damian Pietrzak <preston1989@gmail.com>
2024-03-07 17:46:31 +0100

    Create database schema and start of building website.

--- Day 83 - Website/db_schema.py
+++ Day 83 - Website/db_schema.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String, Text, LargeBinary, DateTime, Boolean
+from sqlalchemy import Integer, String, Text, LargeBinary, DateTime, Boolean, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
@@ -27,8 +27,8 @@ class User(db.Model):
 class BlogPost(db.Model):
     __tablename__ = "posts"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.is"))
-    author = relationship("User", back_populates="user_posts")
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    post_author = relationship("User", back_populates="user_posts")
     parent_id: Mapped[int] = mapped_column(Integer)
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     meta_title: Mapped[str] = mapped_column(String(100), nullable=False)
@@ -40,8 +40,55 @@ class BlogPost(db.Model):
     published_at: Mapped[str] = mapped_column(DateTime)
     content: Mapped[str] = mapped_column(String, nullable=False)
     post_image: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
-    #TODO: uzupełnić po utworzeniu pozostałych tabel
-    post_comments = relationship()
-    post_tags = relationship()
-    post_category = relationship()
+    post_comments = relationship("PostComment", back_populates="parent_post")
+    post_tags = relationship("PostTags", back_populates="parent_post")
+    post_category = relationship("PostCategory", back_populates="parent_post")
 
+
+class PostComment(db.Model):
+    __tablename__ = "post_comments"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    post_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("posts.id"))
+    parent_post = relationship("BlogPost", back_populates="post_comments")
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    comment_author = relationship("User", back_populates="user_comments")
+    parent_id: Mapped[int] = mapped_column(Integer)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    published: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    created_at: Mapped[str] = mapped_column(DateTime, nullable=False)
+    edited_at: Mapped[str] = mapped_column(DateTime)
+    published_at: Mapped[str] = mapped_column(DateTime)
+    content: Mapped[str] = mapped_column(String, nullable=False)
+
+class Category(db.Model):
+    __tablename__ = "categories"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    parent_id: Mapped[int] = mapped_column(Integer)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    meta_title: Mapped[str] = mapped_column(String(100), nullable=False)
+    slug: Mapped[str] = mapped_column(String(100), nullable=False)
+    content: Mapped[str] = mapped_column(String, nullable=False)
+
+
+class PostCategory(db.Model):
+    __tablename__ = "post_category"
+    category_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("categories.id"))
+    post_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("posts.id"))
+    __table_args__ = (PrimaryKeyConstraint("category_id", "post_id", name="id"),)
+    parent_post = relationship("BlogPost", back_populates="post_category")
+
+class Tag(db.Model):
+    __tablename__ = "tags"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    meta_title: Mapped[str] = mapped_column(String(100), nullable=False)
+    slug: Mapped[str] = mapped_column(String(100), nullable=False)
+    content: Mapped[str] = mapped_column(String, nullable=False)
+
+
+class PostTags(db.Model):
+    __tablename__ = "post_tags"
+    tag_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("tags.id"))
+    post_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("posts.id"))
+    __table_args__ = (PrimaryKeyConstraint("tag_id", "post_id", name="id"),)
+    parent_post = relationship("BlogPost", back_populates="post_tags")
--- Day 83 - Website/main.py
+++ Day 83 - Website/main.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 import os
-from db_schema import db
+from db_schema import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+Bootstrap5(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
